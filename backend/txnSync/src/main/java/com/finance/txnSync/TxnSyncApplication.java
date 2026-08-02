9fa0a6f2afee43d4e385793107a62d8c6712a02c
Pankaj Yadav <pankajsinghyadav2004@gmail.com>
2026-08-02 06:02:03 +0000

    CHORE: Initialize Spring Boot project with core package structure and DB config

---
+++ backend/txnSync/src/main/java/com/finance/txnSync/TxnSyncApplication.java
@@ -0,0 +1,13 @@
+package com.finance.txnSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxnSyncApplication.class, args);
+	}
+
+}
